7d5799e98718528cb9ebcf85e98e3151899fc6f9
Atl Arredondo <Atl@Atls-MacBook-Pro.local>
2014-09-17 00:12:08 -0700

    Integration of Bth Transit and Bike Directions
    Use of Transit Directions to calculate bike direction in each of the
    specific steps that the Transit Directions provide.
    Bike Direction Calculations are based on start and end location of the
    Transit Steps

--- js/maps.js
+++ js/maps.js
@@ -15,8 +15,12 @@ function initialize() {
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   directionsDisplay.setMap(map);
-}
 
+  directionsDisplay.setPanel(document.getElementById('directions-panel'));
+  var control = document.getElementById('control');
+  control.style.display = 'block';
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+}
 
 
 
@@ -38,15 +42,79 @@ function calcRoute() {
   var request = {
       origin:start,
       destination:end,
-      travelMode: google.maps.TravelMode.BICYCLING
+      travelMode: google.maps.TravelMode.TRANSIT
   };
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(response);
-      alert("Directions Yeah");
+      //alert(response.routes[0].legs[0]);
+
+
+
     }
+
+    var myRoute = response.routes[0].legs[0];
+ 
+
+
+    
+
+
+ alert("Duration"+ myRoute.duration.text +"  "+myRoute.distance.text + " Number of STEPS" + myRoute.steps.length);
+  for (var i = 0; i < myRoute.steps.length; i++) {
+    
+     
+      //alert(myRoute.steps[i].instructions + "  " +myRoute.steps[i].distance.text);
+      alert("Calculating by steps" + i);
+     var  myRouteBike  = calcRouteBike(myRoute.steps[i].start_location, myRoute.steps[i].end_location);
+      
+  }
+
+
   });
+
+}
+
+function calcRouteBike( Tstart,Tend) {
+  
+  var start = Tstart;
+  var end = Tend
+
+  var myRouteB;
+
+  var requestT = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.TravelMode.BICYCLING
+  };
+
+
+  directionsService.route(requestT, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      //directionsDisplay.setDirections(response);
+      //alert(response.routes[0].legs[0]);
+      alert("Worked for Bike ")
+
+
+    }
+
+    myRouteB = response.routes[0].legs[0];
+ alert(myRouteB.distance.value);
+
+
+ alert(myRouteB.distance.text);
+
+
+
+  });
+
+  return myRouteB;
+
 }
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
